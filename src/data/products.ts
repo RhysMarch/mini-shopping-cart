1df8de2dcd5aaecd7d57ea7adff5f53af70768a9
RhysMarch <c1012902@newcastle.ac.uk>
2026-03-31 22:50:08 +0100

    feat: add product type and product data

---
+++ src/data/products.ts
@@ -0,0 +1,9 @@
+import type { Product } from "../types"
+
+export const products: Product[] = [
+    { id : "apple", name: "Apple", price: 0.60 },
+    { id : "banana", name: "Banana", price: 0.30},
+    { id : "cucumber", name: "Cucumber", price: 0.30},
+    { id : "dates", name: "Dates (200g)", price: 1.80},
+    { id : "eggs", name: "Eggs (6 pack)", price: 1.80},
+]
